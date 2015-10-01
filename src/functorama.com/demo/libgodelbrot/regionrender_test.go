56011dffb6f48a3adb287f0e66a4d161a5490798
John Morrice <john@functorama.com>
2015-10-01 16:33:57 +0100

    Benchmark for region renderer

---
+++ src/functorama.com/demo/libgodelbrot/regionrender_test.go
@@ -0,0 +1,15 @@
+package libgodelbrot 
+
+import (
+    "testing"
+)
+
+func BenchmarkRegionRender(b *testing.B) {
+    config := DefaultConfig()
+    redscale := NewRedscalePalette(DefaultIterations)
+    pic := config.BlankImage()
+    b.ResetTimer()
+    for i := 0; i < b.N; i++ {
+        RegionRenderImage(config, redscale, pic)
+    }
+}
