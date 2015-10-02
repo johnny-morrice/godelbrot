1337d27f15069ec6f78be4b8e76b98faee142fe0
John Morrice <john@functorama.com>
2015-10-02 19:26:04 +0100

    Reverted to old-non-limit aware pretty palette

--- src/functorama.com/demo/libgodelbrot/prettypalette.go
+++ src/functorama.com/demo/libgodelbrot/prettypalette.go
@@ -19,17 +19,14 @@ func NewPrettyPalette(iterateLimit uint8) Palette {
 
 // Cache redscale colour values
 func prettyCacher(limit uint8, index uint8) color.NRGBA {
-    limitF := float64(limit)
-    linear := limitF - float64(index)
-    mid := float64(limit) / 2.0
-    quart := mid / 2.0
-    qx := linear - mid
-    qa := limitF / (mid * mid)
+    linear := float64(limit - index)
+    qx := linear - 122.5
+    qa := 255.0 / (122.5 * 122.5)
     quadratic := qa * qx * qx
-    cx := linear - quart
-    ca := - limitF / (quart * quart * quart)
+    cx := linear - 61.25
+    ca := - 255 / (61.25 * 61.25 * 61.25)
     cubic := ca * cx * cx * cx
-    interval := 255.0 / limitF
+    interval := 255.0 / float64(limit)
     return color.NRGBA{
         R: uint8(linear * interval),
         G: uint8(quadratic * interval),
