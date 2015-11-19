91b9dae8eed2252249864c3fa1356a16169b0729
John Morrice <john@functorama.com>
2015-11-19 03:26:30 +0000

    Flatten interfaces

--- src/functorama.com/demo/nativebase/renderapplication.go
+++ src/functorama.com/demo/nativebase/renderapplication.go
@@ -4,7 +4,11 @@ import (
     "functorama.com/demo/base"
 )
 
+type NativeCoordProvider interface {
+    NativeUserCoords() (complex128, complex128)
+}
+
 type RenderApplication interface {
     base.RenderApplication
-    NativeUserCoords() (complex128, complex128)
+    NativeCoordProvider
 }
