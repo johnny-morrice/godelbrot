5caa5c3cd3f2130f2d2d7b62a709be0a48fad210
John Morrice <john@functorama.com>
2016-01-05 19:15:11 +0000

    Godelbrot render app

--- src/functorama.com/demo/renderbrot/renderbrot.go
+++ src/functorama.com/demo/renderbrot/renderbrot.go
@@ -1,2 +1,40 @@
 package main
 
+import (
+    "image/png"
+    "os"
+    "io"
+    "bytes"
+    "log"
+    "functorama.com/demo/libgodelbrot"
+)
+
+func main() {
+    var input io.Reader = os.Stdin
+    var output io.Writer = os.Stdout
+
+    buff := bytes.Buffer{}
+    count, readErr := buff.ReadFrom(input)
+
+    if readErr != nil {
+        log.Fatal("Read error after ", count, "bytes:", readErr)
+    }
+
+    desc, jsonErr := libgodelbrot.FromJSON(buff.Bytes())
+
+    if jsonErr != nil {
+        log.Fatal("Error decoding JSON:", jsonErr)
+    }
+
+    picture, renderErr := libgodelbrot.Render(desc)
+
+    if renderErr != nil {
+        log.Fatal("Render errror:", renderErr)
+    }
+
+    encodeErr := png.Encode(output, picture)
+
+    if encodeErr != nil {
+        log.Fatal("Encoding error:", encodeErr)
+    }
+}
