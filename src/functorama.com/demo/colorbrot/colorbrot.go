80f467c0a8073ad8e92ebc5a3cde22f738ae76a4
John Morrice <john@functorama.com>
2016-01-23 20:57:53 +0000

    Only use libgodelbrot, not lower level functions

--- src/functorama.com/demo/colorbrot/colorbrot.go
+++ src/functorama.com/demo/colorbrot/colorbrot.go
@@ -1,51 +1,37 @@
 package main
 
 import (
-    "image/png"
-    "image"
-    "os"
     "log"
     "flag"
-    "errors"
-    "fmt"
-    "math"
-    "functorama.com/demo/draw"
-    "functorama.com/demo/base"
+    "os"
+    "image/png"
+    "functorama.com/demo/libgodelbrot"
 )
 
 type commandLine struct {
-    storedPalette  string
-    iterateLimit uint
+    config string
 }
 
 func parseCommand() *commandLine {
     args := &commandLine{}
-    flag.StringVar(&args.storedPalette, "storedPalette",
-        "pretty", "Name of stored palette (pretty|redscale)")
-    flag.UintVar(&args.iterateLimit, "iterateLimit", 255, "Iterate limit [1, 255]")
+    flag.StringVar(&args.config, "config", "(none)", "Path to config file")
     flag.Parse()
     return args
 }
 
-func createStoredPalette(code string, iterlim uint8) (draw.Palette, error) {
-    palettes := map[string]draw.PaletteFactory{
-        "redscale": draw.NewRedscalePalette,
-        "pretty":   draw.NewPrettyPalette,
+func readInfo(args *commandLine) (*libgodelbrot.Info, error) {
+    if args.config == "(none)" {
+        desc := &libgodelbrot.Info{}
+        desc.PaletteType = libgodelbrot.Pretty
+        return desc, nil
+    } else {
+        f, err := os.Open(args.config)
+        if err != nil {
+            return nil, err
+        }
+        defer f.Close()
+        return libgodelbrot.ReadInfo(f)
     }
-    found := palettes[code]
-    if found == nil {
-        return nil, errors.New(fmt.Sprintf("Unknown palette: %v", code))
-    }
-    return found(iterlim), nil
-}
-
-func extractPalette(args *commandLine) (draw.Palette, error) {
-    // Validate first
-    if args.iterateLimit < 1 || args.iterateLimit > 255 {
-        return nil, errors.New("iterateLimit must be between 1 and 255 inclusive")
-    }
-
-    return createStoredPalette(args.storedPalette, uint8(args.iterateLimit))
 }
 
 func main() {
@@ -53,10 +39,10 @@ func main() {
     output := os.Stdout
 
     args:= parseCommand()
-    palette, palErr := extractPalette(args)
+    desc, argErr := readInfo(args)
 
-    if palErr != nil {
-        log.Fatal("Error extracting palette: ", palErr)
+    if argErr != nil {
+        log.Fatal("Error extracting palette: ", argErr)
     }
 
     gray, decErr := png.Decode(input)
@@ -65,23 +51,7 @@ func main() {
         log.Fatal("Error decoding PNG: ", decErr)
     }
 
-    // CAUTION lossy conversion
-    iterlim := uint8(args.iterateLimit)
-    bnd := gray.Bounds()
-    bright := image.NewNRGBA(bnd)
-    scale := float64(0xff) / float64(0xffff)
-    for x := bnd.Min.X; x < bnd.Max.X; x++ {
-        for y := bnd.Min.Y; y < bnd.Max.Y; y++ {
-            bigdiv, _, _, _ := gray.At(x, y).RGBA()
-            invdiv := uint8(math.Floor(scale * float64(bigdiv)))
-            member := base.BaseMandelbrot{
-                InvDivergence: invdiv,
-                InSet: invdiv == iterlim,
-            }
-            col := palette.Color(member)
-            bright.Set(x, y, col)
-        }
-    }
+    bright := libgodelbrot.Recolor(desc, gray)
 
     encErr := png.Encode(output, bright)
 
