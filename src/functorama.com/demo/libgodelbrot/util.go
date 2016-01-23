35db2e91600dbd89d172a2bad4abfead77ff3d08
John Morrice <john@functorama.com>
2016-01-23 22:47:17 +0000

    Calculate precision for emitting bignum

--- src/functorama.com/demo/libgodelbrot/util.go
+++ src/functorama.com/demo/libgodelbrot/util.go
@@ -2,6 +2,7 @@ package libgodelbrot
 
 import (
     "math/big"
+    "log"
 )
 
 // Parse a big.Float
@@ -11,5 +12,29 @@ func parseBig(number string) (*big.Float, error) {
 }
 
 func emitBig(b *big.Float) string {
-    return b.Text('e', -1)
+    return b.Text('e', bitDecAcc(b.MinPrec()))
+}
+
+func bitDecAcc(bits uint) int {
+    const maxi64 = ^uint64(0) >> 1
+    const topi = (^uint(0) >> 1) - 1
+    if uint64(bits) > maxi64 {
+        log.Panic("int64 would overflow")
+    }
+    digits := big.NewInt(int64(bits))
+    two := big.NewInt(2)
+    ten := big.NewInt(10)
+    digits.Exp(two, digits, nil)
+
+    var count int
+    for count = int(1); digits.Cmp(ten) != -1; count++ {
+        digits.Div(digits, ten)
+        // Check for overflow
+        // TODO analytic solution before loop
+        if uint(count) == topi {
+            log.Panic("int would overflow")
+        }
+    }
+
+    return count
 }
