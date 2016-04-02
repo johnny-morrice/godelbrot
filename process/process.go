00c197ebb227620232216f5974551330d7993b61
John Morrice <john@functorama.com>
2016-04-02 08:38:40 +0100

    Add types to io streams

--- process/process.go
+++ process/process.go
@@ -7,22 +7,37 @@ import (
     "github.com/johnny-morrice/pipeline"
 )
 
+type Infow io.Writer
+type Infor io.Reader
+type Pngw io.Writer
+type Pngr io.Reader
+
+func PngBuff() (Pngr, Pngw) {
+    buff := &bytes.Buffer{}
+    return Pngr(buff), Pngw(buff)
+}
+
+func InfoBuff() (Infor, Infow) {
+    buff := &bytes.Buffer{}
+    return Infor(buff), Infow(buff)
+}
+
 // Config creates a new Info, given the args, and sends it to stdout.
-func Config(stdout, stderr io.Writer, args []string) error {
+func Config(stdout Infow, stderr io.Writer, args []string) error {
     config := configbrot(args)
     return runPipeCmd(config, &bytes.Buffer{}, stdout, stderr)
 }
 
 // Render sends a new fractal image to the passed stdout pipe, corresponding to the Info
 // serialized in stdin.
-func Render(stdin io.Reader, stdout, stderr io.Writer) error {
+func Render(stdin Infor, stdout Pngw, stderr io.Writer) error {
     render := renderbrot()
     return runPipeCmd(render, stdin, stdout, stderr)
 }
 
 // ConfigRender sends a new fractal image to the passed stdout pipe, corresponding to configbrot's
 // processing of the args slice.
-func ConfigRender(stdout, stderr io.Writer, args []string) error {
+func ConfigRender(stdout Pngw, stderr io.Writer, args []string) error {
     config := configbrot(args)
     render := renderbrot()
 
@@ -32,14 +47,14 @@ func ConfigRender(stdout, stderr io.Writer, args []string) error {
 }
 
 // Zoom magnifies a section of the Info read from stdin, sending it to stdout.
-func Zoom(stdin io.Reader, stdout, stderr io.Writer, args[]string) error {
+func Zoom(stdin Infor, stdout Infow, stderr io.Writer, args[]string) error {
     zoom := zoombrot(args)
     return runPipeCmd(zoom, stdin, stdout, stderr)
 }
 
 // Zoom reads Info from stdin, and sends a fractal to stdout, returning the magnified Info,
-// serialized as a io.Reader.
-func ZoomRender(stdin io.Reader, stdout, stderr io.Writer, args []string) (io.Reader, error) {
+// serialized as an Infor.
+func ZoomRender(stdin Infor, stdout Pngw, stderr io.Writer, args []string) (Infor, error) {
     zoomBuff := &bytes.Buffer{}
     zoomerr := Zoom(stdin, zoomBuff, stderr, args)
     if zoomerr != nil {
