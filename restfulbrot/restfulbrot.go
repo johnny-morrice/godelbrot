9f2b989cadf35919ce0853b844d21f14a1b6a1ba
John Morrice <john@functorama.com>
2016-04-10 19:59:26 +0100

    Add debug mode to service

--- restfulbrot/restfulbrot.go
+++ restfulbrot/restfulbrot.go
@@ -14,6 +14,7 @@ type params struct {
     jobs uint
     port uint
     bind string
+    debug bool
 }
 
 func main() {
@@ -24,21 +25,33 @@ func main() {
     }
 
     const prefix = "/"
-    r := rest.MakeWebservice(info, args.jobs, prefix)
-    http.Handle(prefix, r)
-
+    h := rest.MakeWebservice(info, args.jobs, prefix)
+    if args.debug {
+        log.Println("Running in debug mode")
+        h = loghandler{handler: h}
+    }
     addr := fmt.Sprintf("%v:%v", args.bind, args.port)
-    httperr := http.ListenAndServe(addr, nil)
+    httperr := http.ListenAndServe(addr, h)
     if httperr != nil {
         log.Fatal(httperr)
     }
 }
 
+type loghandler struct {
+    handler http.Handler
+}
+
+func (lh loghandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+    log.Println(req.URL.RequestURI())
+    lh.handler.ServeHTTP(w, req)
+}
+
 func readArgs() params {
     args := params{}
     flag.UintVar(&args.jobs, "jobs", 1, "Number of concurrent render jobs")
-    flag.UintVar(&args.port, "port", 98989, "Port for webservice")
+    flag.UintVar(&args.port, "port", 9898, "Port for webservice")
     flag.StringVar(&args.bind, "bind", "127.0.0.1", "Interface to bind against")
+    flag.BoolVar(&args.debug, "debug", false, "Verbose logging")
     flag.Parse()
     return args
 }
