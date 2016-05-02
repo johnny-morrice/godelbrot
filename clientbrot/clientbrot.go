27c07ac82778b43cef610c234fb1e0a0bb23a3c1
John Morrice <john@functorama.com>
2016-05-02 21:47:56 +0100

    Use portable restclient lib for #16

--- clientbrot/clientbrot.go
+++ clientbrot/clientbrot.go
@@ -2,28 +2,29 @@ package main
 
 import (
     "bytes"
-    "flag"
     "encoding/json"
+    "flag"
     "fmt"
     "io"
-    "log"
     "net/http"
     "os"
-    "runtime/debug"
     "time"
-    "github.com/johnny-morrice/godelbrot/rest"
+    "github.com/johnny-morrice/godelbrot/restclient"
+    "github.com/johnny-morrice/godelbrot/config"
+    "github.com/johnny-morrice/godelbrot/rest/protocol"
     lib "github.com/johnny-morrice/godelbrot/libgodelbrot"
 )
 
 func main() {
     args := readArgs()
 
-    web := newWebClient(args)
+    shcl := newShellClient(args)
 
     if (args.cycle && args.getrq == "") || args.newrq {
         info, ierr := lib.ReadInfo(os.Stdin)
         fatalguard(ierr)
-        web.req = info.GenRequest()
+        req := info.GenRequest()
+        shcl.args.config.StartReq = &req
     }
 
     zoomparts := map[string]bool {
@@ -36,30 +37,30 @@ func main() {
     flag.Visit(func (fl *flag.Flag) {
         _, ok := zoomparts[fl.Name]
         if ok {
-            web.zoom = true
+            shcl.zoom = true
         }
     })
 
     // Ugly
     var r io.Reader
     if args.cycle {
-        png, err := web.cycle()
+        png, err := shcl.cycle()
         fatalguard(err)
         r = png
     } else if args.newrq {
-        rqi, err := web.newrq()
+        rqi, err := shcl.newrq()
         fatalguard(err)
         reqr, jerr := jsonr(rqi)
         fatalguard(jerr)
         r = reqr
     } else if args.getrq != "" {
-        rqi, err := web.getrq()
+        rqi, err := shcl.getrq()
         fatalguard(err)
         reqr, jerr := jsonr(rqi)
         fatalguard(jerr)
         r = reqr
     } else if args.getimag != "" {
-        png, err := web.getimag()
+        png, err := shcl.getimag()
         fatalguard(err)
         r = png
     }
@@ -68,246 +69,52 @@ func main() {
     fatalguard(cpyerr)
 }
 
-type webclient struct {
-    args params
-    client http.Client
-    req lib.Request
+type shellClient struct {
+    restclient *restclient.Client
     zoom bool
-    tick *time.Ticker
-
+    args params
 }
 
-func newWebClient(args params) *webclient {
-    web := &webclient{}
-    web.args = args
-    web.client.Timeout = time.Millisecond * time.Duration(web.args.timeout)
-    return web
+func newShellClient(args params) *shellClient {
+    shcl := &shellClient{}
+    shcl.args = args
+    hcl := &http.Client{}
+    hcl.Timeout = time.Millisecond * time.Duration(shcl.args.timeout)
+    args.config.Http = (*goHttp)(hcl)
+    return shcl
 }
 
-func (web *webclient) cycle() (io.Reader, error) {
-    // Continue zoom or start anew?
-    var rqurl string
-    if web.args.getrq == "" {
-        newresp, err := web.newrq()
-        if err != nil {
-            return nil, err
-        }
-        rqurl = web.url(newresp.RQStatusURL)
-    } else {
-        if web.zoom {
-            newresp, err := web.rqzoom()
-            if err != nil {
-                return nil ,err
-            }
-            rqurl = web.url(newresp.RQStatusURL)
-        } else {
-            rqurl = web.rqurl()
-        }
+func (shcl *shellClient) cycle() (io.Reader, error) {
+    const url = ""
+    var target *config.ZoomBounds
+    if shcl.zoom {
+        target = &shcl.args.zoombox
     }
-    for {
-        rqstat, err := web.getrqraw(rqurl)
-        if err != nil {
-            return nil, err
-        }
-        switch rqstat.State {
-        case "done":
-            imgurl := web.url(rqstat.ImageURL)
-            return web.getimagraw(imgurl)
-        case "error":
-            weberr := fmt.Errorf("RQGetResp error: %v", rqstat.Error)
-            return nil, weberr
-        case "wait":
-            // NOP
-        default:
-            panic(fmt.Errorf("Unknown status: %v", rqstat.State))
-        }
+    return shcl.restclient.Cycle(url, shcl.zoom, target)
+}
+
+func (shcl *shellClient) getimag() (io.Reader, error) {
+    url := fmt.Sprintf("image/%v", shcl.args.getimag)
+    return shcl.restclient.Getimag(shcl.restclient.Url(url))
+}
+
+func (shcl *shellClient) newrq() (*protocol.RQNewResp, error) {
+    renreq := &protocol.RenderRequest{}
+    renreq.Req = *shcl.args.config.StartReq
+    if shcl.zoom {
+        renreq.WantZoom = shcl.zoom
+        renreq.Target = shcl.args.zoombox
     }
+    return shcl.restclient.Newrq(shcl.restclient.Url("renderqueue"), renreq)
 }
 
-func (web *webclient) newrq() (*rest.RQNewResp, error) {
-    return web.newrqraw(web.url("renderqueue"), web.renreq())
+func (shcl *shellClient) getrq() (*protocol.RQGetResp, error) {
+    url := shcl.rqurl()
+    return shcl.restclient.Getrq(url)
 }
 
-func (web *webclient) newrqraw(url string, renreq *rest.RenderRequest) (*rest.RQNewResp, error) {
-    buff := &bytes.Buffer{}
-    werr := rest.WriteReq(buff, renreq)
-    if werr != nil {
-        return nil, addstack(werr)
-    }
-    resp, err := web.post(url, "application/json", buff)
-    if err != nil {
-        return nil, err
-    }
-    if resp.StatusCode != 200 {
-        return nil, httpError(resp)
-    }
-    defer resp.Body.Close()
-    rqi := &rest.RQNewResp{}
-    derr := web.decode(resp.Body, rqi)
-    return rqi, addstack(derr)
-}
-
-func (web *webclient) rqzoom() (*rest.RQNewResp, error) {
-    cacheresp, err := web.getrq()
-    if err != nil {
-        return nil, err
-    }
-    renreq := &rest.RenderRequest{}
-    if !web.zoom {
-        fmt.Fprintf(os.Stderr, "Warning: Pointless rqzoom with zoom off")
-    }
-    renreq.WantZoom = web.zoom
-    renreq.Target.Xmin = web.args.xmin
-    renreq.Target.Xmax = web.args.xmax
-    renreq.Target.Ymin = web.args.ymin
-    renreq.Target.Ymax = web.args.ymax
-    renreq.Req = cacheresp.NextReq
-    return web.newrqraw(web.url("renderqueue"), renreq)
-}
-
-func (web *webclient) getrq() (*rest.RQGetResp, error) {
-    url := web.rqurl()
-    return web.getrqraw(url)
-}
-
-func (web *webclient) rqurl() string {
-    return web.url(fmt.Sprintf("renderqueue/%v", web.args.getrq))
-}
-
-func (web *webclient) getrqraw(url string) (*rest.RQGetResp, error) {
-    resp, err := web.get(url)
-    if err != nil {
-        return nil, err
-    }
-    defer resp.Body.Close()
-    rqi := &rest.RQGetResp{}
-    derr := web.decode(resp.Body, rqi)
-    return rqi, addstack(derr)
-}
-
-func (web *webclient) getimag() (io.Reader, error) {
-    url := fmt.Sprintf("image/%v", web.args.getimag)
-    return web.getimagraw(web.url(url))
-}
-
-func (web *webclient) getimagraw(url string) (io.Reader, error) {
-    resp, err := web.get(url)
-    if err != nil {
-        return nil, err
-    }
-    if resp.StatusCode != 200 {
-        return nil, httpError(resp)
-    }
-    defer resp.Body.Close()
-    buff := &bytes.Buffer{}
-    _, cpyerr := io.Copy(buff, resp.Body)
-    return buff, addstack(cpyerr)
-}
-
-func (web *webclient) renreq() *rest.RenderRequest {
-    renreq := &rest.RenderRequest{}
-    renreq.Req = web.req
-    renreq.WantZoom = web.zoom
-    renreq.Target.Xmin = web.args.xmin
-    renreq.Target.Xmax = web.args.xmax
-    renreq.Target.Ymin = web.args.ymin
-    renreq.Target.Ymax = web.args.ymax
-
-    return renreq
-}
-
-func (web *webclient) url(last string) string {
-    args := web.args
-    if web.args.prefix == "" {
-        return fmt.Sprintf("http://%v:%v/%v/",
-            args.addr, args.port, last)
-    } else {
-        return fmt.Sprintf("http://%v:%v/%v/%v",
-                args.addr, args.port, args.prefix, last)
-    }
-}
-
-func (web *webclient) get(url string) (r *http.Response, err error) {
-    web.cautiously(func () {
-        if web.args.debug {
-            log.Printf("GET %v", url)
-        }
-        r, err = web.client.Get(url)
-        if web.args.debug {
-            web.reportResponse(r, err)
-        }
-    })
-    return
-}
-
-func (web *webclient) post(url, ctype string, body io.Reader) (r *http.Response, err error) {
-    web.cautiously(func () {
-        if web.args.debug {
-            log.Printf("POST %v", url)
-        }
-        r, err = web.client.Post(url, ctype, body)
-        if web.args.debug {
-            web.reportResponse(r, err)
-        }
-    })
-    return
-}
-
-func (web *webclient) reportResponse(r *http.Response, err error) {
-    if err != nil {
-        log.Printf("Error: %v", err)
-    }
-    if r == nil {
-        return
-    }
-    log.Printf("Status: %v", r.Status)
-    ctypeHeads := r.Header["Content-Type"]
-    if len(ctypeHeads) != 1 {
-        log.Printf("Bad Content-Type header")
-    } else {
-        log.Printf("Content-Type: %v", ctypeHeads[0])
-    }
-}
-
-func (web *webclient) cautiously(f func()) {
-    if web.tick == nil {
-        web.tick = time.NewTicker(time.Duration(web.args.ticktime) * time.Millisecond)
-    } else {
-        <-web.tick.C
-    }
-    f()
-}
-
-func (web *webclient) decode(r io.Reader, any interface{}) error {
-    if web.args.debug {
-        buff := &bytes.Buffer{}
-        r = io.TeeReader(r, buff)
-        derr := decode(r, any)
-        log.Printf("Decoded: %v", buff.String())
-        return derr
-    }
-    return decode(r, any)
-}
-
-func httpError(resp *http.Response) error {
-    buff := &bytes.Buffer{}
-    err := resp.Write(buff)
-    if err != nil {
-        panic(err)
-    }
-    return fmt.Errorf("Response:\n%v", buff)
-}
-
-func decode(r io.Reader, any interface{}) error {
-    dec := json.NewDecoder(r)
-    return dec.Decode(any)
-}
-
-func jsonr(any interface{}) (io.Reader, error) {
-    buff := &bytes.Buffer{}
-    enc := json.NewEncoder(buff)
-    err := enc.Encode(any)
-    return buff, err
+func (shcl *shellClient) rqurl() string {
+    return shcl.restclient.Url(fmt.Sprintf("renderqueue/%v", shcl.args.getrq))
 }
 
 func fatalguard(err error) {
@@ -317,33 +124,24 @@ func fatalguard(err error) {
     }
 }
 
-func addstack(err error) error {
-    if err == nil {
-        return nil
-    } else {
-        return fmt.Errorf("%v\n%v", err, string(debug.Stack()))
-    }
-
-}
-
-
 func readArgs() params {
     args := params{}
-    flag.StringVar(&args.addr, "remote", "localhost", "Remote address of restfulbrot service")
-    flag.UintVar(&args.port, "port", 9898, "Port of remote service")
-    flag.StringVar(&args.prefix, "prefix", "", "Prefix of service URL")
+    flag.StringVar(&args.config.Addr, "remote", "localhost", "Remote address of restfulbrot service")
+    flag.UintVar(&args.config.Port, "port", 9898, "Port of remote service")
+    flag.StringVar(&args.config.Prefix, "prefix", "", "Prefix of service URL")
+    flag.UintVar(&args.config.Ticktime, "ticktime", 100, "Max one request per tick (milliseconds)")
+    flag.BoolVar(&args.config.Debug, "debug", false, "Verbose debug mode")
     flag.UintVar(&args.timeout, "timeout", 1000, "Web request abort timeout (milliseconds)")
-    flag.UintVar(&args.ticktime, "ticktime", 100, "Max one request per tick (milliseconds)")
     flag.BoolVar(&args.newrq, "newrq", false, "Add new item to render queue (info from stdin)")
     flag.StringVar(&args.getrq, "getrq", "", "Get status of render queue item")
     flag.StringVar(&args.getimag, "getimag", "", "Download fractal render (png to stdout)")
     flag.BoolVar(&args.cycle, "cycle", false,
         "Wait for fractal to render (info from stdin, png to stdout")
-    flag.BoolVar(&args.debug, "debug", false, "Verbose debug mode")
-    flag.UintVar(&args.xmin, "xmin", 0, "xmin pixel bound")
-    flag.UintVar(&args.xmax, "xmax", 0, "xmax pixel bound")
-    flag.UintVar(&args.ymin, "ymin", 0, "ymin pixel bound")
-    flag.UintVar(&args.ymax, "ymax", 0, "ymax pixel bound")
+
+    flag.UintVar(&args.zoombox.Xmin, "xmin", 0, "xmin pixel bound")
+    flag.UintVar(&args.zoombox.Xmax, "xmax", 0, "xmax pixel bound")
+    flag.UintVar(&args.zoombox.Ymin, "ymin", 0, "ymin pixel bound")
+    flag.UintVar(&args.zoombox.Ymax, "ymax", 0, "ymax pixel bound")
     flag.Parse()
 
     // Cycle is default on only if no other operations provided.
@@ -367,21 +165,19 @@ func readArgs() params {
 }
 
 type params struct {
-    addr string
-    port uint
-    prefix string
-    timeout uint
-    ticktime uint
-    debug bool
-
+    config restclient.Config
     newrq bool
     getrq string
     getimag string
     cycle bool
+    timeout uint
 
-    config string
-    xmin uint
-    xmax uint
-    ymin uint
-    ymax uint
+    zoombox config.ZoomBounds
+}
+
+func jsonr(any interface{}) (io.Reader, error) {
+    buff := &bytes.Buffer{}
+    enc := json.NewEncoder(buff)
+    err := enc.Encode(any)
+    return buff, err
 }
